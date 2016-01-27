8f68b421d64212e30b16f060c9b8147b3a913351
Matt Waltz <matthewwaltzis@gmail.com>
2016-01-26 23:35:44 -0800

    Added license to main file

--- libload.asm
+++ libload.asm
@@ -1,5 +1,15 @@
 ;=========================================================================
-;  Copyright (C) 2015 Matt Waltz.  All rights reserved.
+; Copyright (C) 2015 Matt Waltz.  All rights reserved.
+;
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
 ;=========================================================================
 ; Performs dynamic relocation for shared libraries and interdependencies
 ; inputs: de->start of library relocation table
