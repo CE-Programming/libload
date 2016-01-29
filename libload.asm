77cf1c42e9d5bae79c90f091cf2eb6a6e7eebc93
Matt Waltz <matthewwaltzis@gmail.com>
2016-01-28 16:31:51 -0800

    Changed License to LGPL v3

--- libload.asm
+++ libload.asm
@@ -1,16 +1,13 @@
 ;=========================================================================
-; Copyright (C) 2015 Matt Waltz.  All rights reserved.
+; Copyright (C) 2015 Matt Waltz
 ; Version 1.0
 ;
-; This program is free software: you can redistribute it and/or modify
-; it under the terms of the GNU General Public License as published by
-; the Free Software Foundation, either version 3 of the License, or
-; (at your option) any later version.
-;
-; This program is distributed in the hope that it will be useful,
-; but WITHOUT ANY WARRANTY; without even the implied warranty of
-; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-; GNU General Public License for more details.
+; This library is free software; you can redistribute it and/or
+; modify it under the terms of the GNU Lesser General Public
+; License as published by the Free Software Foundation; either
+; version 3 of the License, or (at your option) any later version.
+; See the file COPYING included with this distribution for more
+; information.
 ;=========================================================================
 ; Performs dynamic relocation for shared libraries and interdependencies
 ; inputs: de->start of library relocation table
